83609ab012f42cfefd85f71a418d15e79782ec81
KIRAY <lol@kremlin.ru>
2017-11-23 11:21:34 +0300

    Chehol

--- EP.cpp
+++ EP.cpp
@@ -13,4 +13,26 @@ Author:
 
 --*/
 
-void main() {}
+#include <iostream>
+
+using namespace std;
+
+void main() {
+	int menu = 0;
+
+	cout << "Calculator 1337" << endl;
+
+	cout << "Choose action: \n\t1) Add\n\t2) Sub\n\t3) Mul\n\t4) Div" << endl;
+	cin >> menu;
+
+	switch(menu) {
+		case 1:
+			break;
+		case 2:
+			break;
+		case 3:
+			break;
+		case 4:
+			break;
+	}
+}
